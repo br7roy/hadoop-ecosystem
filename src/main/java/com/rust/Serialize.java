6437d4478fa129d2708f7d91c41f38725e137b09
Rust <br7roy@gmail.com>
2018-11-10 12:05:13 +0800

    Avro ,串行化反串行化

---
+++ src/main/java/com/rust/Serialize.java
@@ -0,0 +1,37 @@
+package com.rust;
+
+import com.rust.myavro.model.User;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.io.DatumWriter;
+import org.apache.avro.specific.SpecificDatumWriter;
+
+import java.io.File;
+
+/**
+ * 串行
+ * @author Rust
+ */
+public class Serialize {
+	public static void main(String[] args) throws Exception{
+		User user =
+				User.newBuilder().setFavoriteColor("black").setFavoriteNumber(7).setName("zhangsan").build();
+		User user2 = User.newBuilder()
+				.setName("tom2")
+				.setFavoriteNumber(77)
+				.setFavoriteColor("black")
+				.build();
+
+
+		DatumWriter<User> userDatumWriter = new SpecificDatumWriter<User>(User.class);
+		DataFileWriter<User> dataFileWriter = new DataFileWriter<User>(userDatumWriter);
+
+
+		dataFileWriter.create(user.getSchema(), new File("users.avro"));
+		dataFileWriter.append(user);
+		dataFileWriter.append(user2);
+		dataFileWriter.close();
+		System.out.println("over");
+
+
+	}
+}
