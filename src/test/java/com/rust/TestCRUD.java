d881f29322fc14507dacd8eee9c9b0d36774f90c
Rust <br7roy@gmail.com>
2018-11-21 00:22:01 +0800

    Initial commit

---
+++ src/test/java/com/rust/TestCRUD.java
@@ -0,0 +1,19 @@
+ /*
+  * Package com.rust
+  * FileName: TestCRUD
+  * Author:   Takho
+  * Date:     18/11/21 0:17
+  */
+ package com.rust;
+
+ import junit.framework.TestCase;
+
+ /**
+  * @author Takho
+  */
+ public class TestCRUD extends TestCase {
+
+
+
+
+ }
