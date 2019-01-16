5de42795735c2276cf2e9a41c6673ecaaa8ec168
Rust <br7roy@gmail.com>
2019-01-17 00:11:18 +0800

    merge

---
+++ src/main/scala/com/rust/one/WcApp.scala
@@ -0,0 +1,17 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import java.net.URL
+
+object WcApp {
+  def main(args:Array[String]){
+    val conf = new SparkConf().setAppName("wordCount").setMaster("local[4]").set("fs.hdfs.impl","org.apache.hadoop.hdfs.DistributedFileSystem")
+
+    val sc = new SparkContext(conf)
+    val input = sc.textFile("file:/home/ubuntu/devicestatus.txt")
+    val words = input.flatMap(line => line.split(","))
+    //  Transform into pairs and count
+    val counts = words.map(word => (word,1)).reduceByKey{case (x,y) => x+y }
+    //  save the word count back out to a text file, causing evaluation
+    counts.saveAsTextFile("file:/home/ubuntu/out.txt")
+  }
+}
