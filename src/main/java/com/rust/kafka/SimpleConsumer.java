45e6b91e049d63c99ab6976767c43f77c6ecb955
Rust <br7roy@gmail.com>
2019-03-05 22:41:17 +0800

    模拟consumer 从头读取消息

--- src/main/java/com/rust/kafka/SimpleConsumer.java
+++ src/main/java/com/rust/kafka/SimpleConsumer.java
@@ -26,18 +26,17 @@
   */
  public class SimpleConsumer {
 	 public static void main(String[] args) {
-
-
 		 Properties properties = new Properties();
 		 properties.put("zookeeper.connect", "s101:2181");
 		 properties.put("group.id", "g1");
 		 properties.put("zookeeper.session.timeout.ms", "5000");
 		 properties.put("zookeeper.sync.time.ms", "250");
 		 properties.put("auto.commit.interval.ms", "1000");
-		 properties.put("auto.offset.reset", "smallest");
 
 		 //	删除节点偏移量消费信息，这样就从头开始读取数据了
 		 ZkUtils.maybeDeletePath("s101:2181", "/consumers/" + "g1");
+		 //	这个值代表偏移量的重置位置，设为smallest代表从头开始读取
+		 properties.put("auto.offset.reset", "smallest");
 		 // 创建消费者配置对象
 		 ConsumerConfig consumerConfig = new ConsumerConfig(properties);
 
