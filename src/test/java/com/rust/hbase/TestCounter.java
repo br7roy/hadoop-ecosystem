f947b16538daf6238eca0776f4687b342d879b63
Rust <br7roy@gmail.com>
2019-03-24 21:04:24 +0800

    单个计数器，多计数器

--- src/test/java/com/rust/hbase/TestCounter.java
+++ src/test/java/com/rust/hbase/TestCounter.java
@@ -14,6 +14,7 @@ import org.apache.hadoop.hbase.client.Admin;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Increment;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.junit.After;
 import org.junit.Before;
@@ -21,6 +22,7 @@ import org.junit.Test;
 
 /**
  * 计数器
+ *
  * @author Takho
  */
 public class TestCounter {
@@ -43,10 +45,11 @@ public class TestCounter {
 
 
 	/**
+	 * 单个计数器
 	 * 点击量可以用这个方法计算
 	 */
 	@Test
-	public void counter1() throws Exception{
+	public void counter1() throws Exception {
 		HTable t = (HTable) connection.getTable(TableName.valueOf("ns1:t3"));
 		// Increment increment = new Increment(Bytes.toBytes("row1"));
 		long l = t.incrementColumnValue(Bytes.toBytes("row1"), Bytes.toBytes("cf1"), Bytes.toBytes("no"), 1L);
@@ -54,13 +57,23 @@ public class TestCounter {
 		// Result increment1 = t.increment(increment);
 		// System.out.println(increment1);
 		t.close();
-
-
 	}
 
 
-
-
+	/**
+	 * 多计数器
+	 */
+	@Test
+	public void counterN() throws Exception {
+		HTable table = (HTable) connection.getTable(TableName.valueOf("ns1:t3"));
+		Increment incr = new Increment(Bytes.toBytes("row1"));
+		incr.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("no"), 1);
+		incr.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("no1"), 2);
+		incr.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("no2"), 3);
+		incr.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("no3"), 4);
+		table.increment(incr);
+		table.close();
+	}
 
 
 }
