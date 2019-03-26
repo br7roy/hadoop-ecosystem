c1fd06136eb5a5d665d287cab3cb09c65b5dc77f
Rust <br7roy@gmail.com>
2019-03-26 22:38:07 +0800

    Merge branch 'master' of gitlab.com:br7roy/mykundera
    # Conflicts:
    #	pom.xml
    #	src/main/java/com/rust/kundera/App.java

--- src/main/java/com/rust/kundera/App.java
+++ src/main/java/com/rust/kundera/App.java
@@ -1,23 +1,7 @@
 package com.rust.kundera;
 
-import entity.T1;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
-/**
- * Hello world!
- */
 public class App {
 	public static void main(String[] args) {
-		EntityManagerFactory hbase_pu = Persistence.createEntityManagerFactory("hbase_pu");
-		EntityManager entityManager = hbase_pu.createEntityManager();
-		T1 t1 = new T1();
-		t1.setAge(80);
-		t1.setNo("001");
-		t1.setName("tomasLee");
-		entityManager.persist(t1);
-
+		System.out.println("hello world !");
 	}
 }
--- src/main/java/entity/T1.java
+++ src/main/java/entity/T1.java
@@ -12,6 +12,8 @@
  */
 package entity;
 
+import com.google.common.base.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -21,14 +23,28 @@ import javax.persistence.Table;
  * @author Rust
  */
 @Entity
-@Table(name = "t1")
-public class T1 {
+@Table(name = "cf1")
+/** 对应列簇 column_family */ public class T1 {
+
+	/**
+	 * 这个是rowkey
+	 */
 	@Id
-	private String no;
+	private String rowKey;
 	@Column
 	private String name;
 	@Column
 	private int age;
+	@Column
+	private String no;
+
+	public String getRowKey() {
+		return rowKey;
+	}
+
+	public void setRowKey(String rowKey) {
+		this.rowKey = rowKey;
+	}
 
 	public String getName() {
 		return name;
@@ -36,6 +52,14 @@ public class T1 {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
 	}
 
 	public String getNo() {
@@ -46,11 +70,8 @@ public class T1 {
 		this.no = no;
 	}
 
-	public int getAge() {
-		return age;
-	}
-
-	public void setAge(int age) {
-		this.age = age;
+	@Override
+	public String toString() {
+		return Objects.toStringHelper(this).add("rowKey", rowKey).add("name", name).add("age", age).add("no", no).toString();
 	}
 }
