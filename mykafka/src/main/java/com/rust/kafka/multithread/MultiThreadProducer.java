6e5cee0f0bc65d1de521422f3130a442e93860ae
Tak <br7roy@gmail.com>
2019-08-17 20:53:47 +0800

    balance

---
+++ mykafka/src/main/java/com/rust/kafka/multithread/MultiThreadProducer.java
@@ -0,0 +1,45 @@
+/*
+ * Package:  com.rust.kafka.multithread
+ * FileName: MultiThreadProducer
+ * Author:   Tak
+ * Date:     23/7/2019 下午3:17
+ * email:    br7roy@gmail.com
+ */
+package com.rust.kafka.multithread;
+
+import java.util.Properties;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * reference https://blog.csdn.net/u010003835/article/details/83000537
+ * @author Takho
+ */
+public class MultiThreadProducer {
+	public static final MultiThreadProducer ONE = new MultiThreadProducer();
+	private final ThreadPoolExecutor poolExecutor;
+
+	public MultiThreadProducer() {
+		poolExecutor = new ThreadPoolExecutor(2, 4, 60, TimeUnit.SECONDS, new ArrayBlockingQueue<>(300));
+		Runtime.getRuntime().addShutdownHook(new Thread(poolExecutor::shutdown));
+	}
+
+	public static MultiThreadProducer getONE() {
+		return ONE;
+	}
+
+	static class MultiProduceWorker implements Runnable {
+
+		@Override
+		public void run() {
+
+		}
+	}
+
+	public static void main(String[] args) {
+		Properties p = new Properties();
+
+
+	}
+}
